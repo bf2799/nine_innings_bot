5631d99ce2e50ac1bceb84213613b098fbda76a7
Ben Fox <astudent@cinderhippo.com>
2022-12-24 22:07:25 -0500

    Added scout command

--- src/main.py
+++ src/main.py
@@ -3,6 +3,9 @@
 import argparse
 
 import discord
+from discord.ext.commands.cooldowns import BucketType, Cooldown, CooldownMapping
+
+from src.scouter import Scouter
 
 bot = discord.Bot()  # type: ignore
 
@@ -17,6 +20,59 @@ async def help(context: discord.ApplicationContext) -> None:
     await context.respond(
         "**/help**: List all available commands and their descriptions\n"
     )
+
+
+@bot.command(
+    description="Get PR and OVR of teams provided from scouting database",
+    cooldown=CooldownMapping(Cooldown(rate=2, per=21600), BucketType.channel),
+)  # type: ignore
+@discord.option("teams", description="List of teams to retrieve, separated by spaces")  # type: ignore
+async def scout(
+    context: discord.ApplicationContext,
+    teams: str,
+):
+    """
+    Get PR and OVR of teams provided from scouting database.
+
+    :param context: Application context
+    :param teams: List of teams to search, as space-separated strings
+    """
+    # Split teams by space
+    team_list = teams.split(" ")
+    k_max_teams = 25
+    if len(team_list) > k_max_teams:
+        await context.respond(
+            f"Too many teams entered ({len(team_list)}). Maximum is {k_max_teams}."
+        )
+        return
+    # Get scouting teams then format
+    team_info = Scouter.read_scouting(team_list)
+    border_str = "-" * 54
+    header_str = f"| {'Team' : ^20} | {'Date' : ^10} | {'OVR': ^5} | {'PR': ^6} |"
+    body_str = "\n".join(
+        [
+            f"| {info[0] : ^20} | {info[1] : ^10} | {info[2] : ^5} | {info[3] : ^6} |"
+            for info in team_info
+        ]
+    )
+    response = (
+        f"```\n{border_str}\n{header_str}\n{border_str}\n{body_str}\n{border_str}\n```"
+    )
+    # Send formatted teams to Discord
+    await context.respond(response)
+
+
+@bot.event
+async def on_application_command_error(
+    context: discord.ApplicationContext, error: str
+) -> None:
+    """
+    Provide user with error.
+
+    :param context: Application context
+    :param error: Error that occurred
+    """
+    await context.respond(error)
 
 
 if __name__ == "__main__":
---
+++ src/scouter.py
@@ -0,0 +1,107 @@
+"""Module that connects to scouting database."""
+
+import os
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+
+
+class Scouter(object):
+    """
+    Class that uses Google Sheets integration for inserting and retrieving scouting data.
+
+    Class is a singleton to ensure there is only a single connection to sheet.
+    """
+
+    _SCOPES: list[str] = ["https://www.googleapis.com/auth/spreadsheets"]
+    _SHEET_ID: str = "1xjdF3dZ15q_FyUYUlMa0sAiMEYTnV51_9NRJSebeD7Y"
+    _CREDENTIAL_FILEPATH: str = "input/google_sheets_credentials.json"
+    _TOKEN_FILEPATH: str = "input/google_sheets_token.json"
+    _connected: bool = False
+    _sheet = None
+
+    _COLUMNS: dict[str, int] = {
+        "team": 0,
+        "date": 1,
+        "ovr": 2,
+        "pr": 3,
+    }
+
+    @classmethod
+    def _connect(cls) -> None:
+        """Connect to Google Sheets service."""
+        creds = None
+        if os.path.exists(cls._TOKEN_FILEPATH):
+            creds = Credentials.from_authorized_user_file(
+                cls._TOKEN_FILEPATH, cls._SCOPES
+            )
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    cls._CREDENTIAL_FILEPATH, cls._SCOPES
+                )
+                creds = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+            with open(cls._TOKEN_FILEPATH, "w") as token:
+                token.write(creds.to_json())
+
+        try:
+            service = build("sheets", "v4", credentials=creds)
+            cls._sheet = service.spreadsheets()
+            cls._connected = True
+        except Exception as e:
+            cls._connected = False
+            raise RuntimeError("Scouter could not connect to scouting sheet") from e
+
+    @classmethod
+    def read_scouting(cls, team_names: list[str]) -> list[tuple[str, ...]]:
+        """
+        Read scouting data from Google sheets
+
+        :param team_names: Teams to gather from scouting sheet
+        :raises RuntimeError: Data could not be properly attained, via connection error or other
+        :return: Scouting data formatted as list of (team, date, ovr, pr)
+        """
+        # Try connecting. On failure, exception will be raised, and this will be exited
+        if not cls._connected:
+            cls._connect()
+        if not cls._sheet:
+            raise RuntimeError(
+                "Connection to scouting database could be properly established"
+            )
+        # Get lowercase team names
+        lower_team_names = [name.lower() for name in team_names]
+        tuple_teams: list[tuple[str, ...]] = []
+        # Try running sheet operations. If it fails, try connecting again. Total only twice
+        for i in range(2):
+            try:
+                result = (
+                    cls._sheet.values()
+                    .get(spreadsheetId=cls._SHEET_ID, range="Scouting!A2:D")
+                    .execute()
+                )
+                all_values = result.get("values", [])
+                filtered_teams = [
+                    teams
+                    for teams in [all_values][0]
+                    if teams[cls._COLUMNS["team"]].lower() in lower_team_names
+                ]
+                # Turn trailing empty cells into empty strings
+                for team in filtered_teams:
+                    team_vals = [""] * len(cls._COLUMNS)
+                    team_vals[: len(team)] = team
+                    tuple_teams.append(tuple(team_vals))
+                # Stop after first attempt if successful
+                break
+            except Exception:
+                cls._connect()
+        # Tell user if connection failed
+        if not tuple_teams:
+            raise RuntimeError(
+                "Connection to scouting database could be properly established"
+            )
+        return tuple_teams
